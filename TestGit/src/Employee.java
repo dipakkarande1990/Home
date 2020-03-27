f5d22edfd023df4dd8881d74a010b3f66d57f1c7
Dipak Karande <dipakkarande1990@gmail.com>
2020-03-27 09:38:32 +0530

    second

---
+++ TestGit/src/Employee.java
@@ -0,0 +1,38 @@
+
+public class Employee {
+	
+	private int empId;
+	private String empName;
+	private String address;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
